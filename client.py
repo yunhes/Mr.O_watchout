ad9c12fd805bfed172351209ff23319659d04a5b
yunhes <yunhes@uci.edu>
2020-05-28 01:23:32 -0700

    initial version

--- client.py
+++ client.py
@@ -3,11 +3,14 @@ import numpy as np
 import socket
 import sys
 import pickle
+from picamera import PiCamera
 import struct ### new code
-cap=cv2.VideoCapture(0)
+
+camera = cv2.VideoCapture(0)
+
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+clientsocket.connect(('192.168.31.60',8089))
 while True:
-    ret,frame=cap.read()
+    ret,frame=camera.read()
     data = pickle.dumps(frame) ### new code
-    clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
+    clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
